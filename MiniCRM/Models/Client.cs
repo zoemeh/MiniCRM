bf8da9a8a833f25e6de940c6aa95a7f78fff7d7e
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2022-05-11 20:46:23 -0400

    Add Client model

---
+++ MiniCRM/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniCRM.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
