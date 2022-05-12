8546bc252ec72bede39710e77cd1d40af550877d
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2022-05-11 20:53:11 -0400

    Add: validations for Client and ClientAddress model

--- MiniCRM/Models/Client.cs
+++ MiniCRM/Models/Client.cs
@@ -1,9 +1,15 @@
 ﻿namespace MiniCRM.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     public class Client
     {
         public int Id { get; set; }
+        
+        [Required]
+        // No StringLenght validations because there are actually names that are only 1 character and there's not upper limit.
         public string Name { get; set; }
+        [Required]
         public bool IsActive { get; set; }
 
     }
--- MiniCRM/Models/ClientAddress.cs
+++ MiniCRM/Models/ClientAddress.cs
@@ -1,12 +1,19 @@
 ﻿namespace MiniCRM.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     public class ClientAddress
     {
         public int Id { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string ZipCode { get; set; }
+        [Required]
         public string Country { get; set; }
+        [Required]
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
     }
