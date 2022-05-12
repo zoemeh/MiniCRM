2578f9a84d1d9093538b2aeb856b9014bbb09ee6
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2022-05-11 20:44:23 -0400

    init

---
+++ MiniCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
